a7407669f87f1076180f579030a99d1f6f33d41c
Paulsig007 <131915869+Paulsig007@users.noreply.github.com>
2023-06-19 22:05:57 -0600

    add delete path to delete notes from application

--- server.js
+++ server.js
@@ -5,43 +5,91 @@ const { v4: uuidv4 } = require("uuid");
 
 const PORT = 3001;
 
+const dbPath = "./db/db.json";
+const publicDir = path.join(__dirname, "/public");
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(publicDir));
 
-app.use(express.static("public"));
+app.get("/", (req, res) => {
+  const indexPath = path.join(publicDir, "index.html");
+  res.sendFile(indexPath);
+});
 
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
-);
+app.get("/notes", (req, res) => {
+  const notesPath = path.join(publicDir, "notes.html");
+  res.sendFile(notesPath);
+});
 
-app.get("/api/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/notes.html"))
-);
+app.get("/api/notes", (req, res) => {
+  fs.readFile(dbPath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Internal Server Error");
+    }
+    console.log(req.body);
+    const parsedNotes = JSON.parse(data);
+    res.json(parsedNotes);
+  });
+});
 
 app.post("/api/notes", (req, res) => {
   console.info(`${req.method} request received to add a note`);
 
-  fs.readFile("./db/db.json", "utf8", (err, data) => {
+  fs.readFile(dbPath, "utf8", (err, data) => {
     if (err) {
       console.error(err);
-    } else {
-      console.log(req.body);
-      const parsedNotes = JSON.parse(data);
-      const newNote = req.body;
-      req.body.id = parsedNotes.length + 1;
-      parsedNotes.push(newNote);
-
-      fs.writeFile("./db/db.json", JSON.stringify(parsedNotes), (writeErr) =>
-        writeErr
-          ? console.error(writeErr)
-          : console.info("Successfully updated notes!")
-      );
+      return res.status(500).send("Internal Server Error");
     }
+    console.log(req.body);
+    const parsedNotes = JSON.parse(data);
+    const newNote = req.body;
+    newNote.id = uuidv4();
+    parsedNotes.push(newNote);
+
+    fs.writeFile(dbPath, JSON.stringify(parsedNotes, null, 4), (writeErr) => {
+      if (writeErr) {
+        console.error(writeErr);
+        return res.status(500).send("Internal Server Error");
+      }
+      console.info("Successfully updated notes!");
+      res.sendStatus(200);
+    });
   });
 });
 
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
+app.delete("/api/notes/:id", (req, res) => {
+  const noteId = req.params.id;
+
+  fs.readFile(dbPath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Internal Server Error");
+    }
+
+    let parsedNotes = JSON.parse(data);
+    const filteredNotes = parsedNotes.filter((note) => note.id !== noteId);
+
+    if (parsedNotes.length === filteredNotes.length) {
+      return res.status(404).send("Note not found");
+    }
+
+    parsedNotes = filteredNotes;
+
+    fs.writeFile(dbPath, JSON.stringify(parsedNotes, null, 4), (writeErr) => {
+      if (writeErr) {
+        console.error(writeErr);
+        return res.status(500).send("Internal Server Error");
+      }
+      console.info("Successfully deleted the note!");
+      res.sendStatus(204);
+    });
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT} 🚀`);
+});
